ba8e3ab8aa4277eb320b486facc4cff095ae70b4
bjuppa <bjorn.nilsved@fewagency.se>
2016-11-17 11:59:02 +0100

    Put basic inline semantic text in a partial for reuse

--- resources/components/typography.php
+++ resources/components/typography.php
@@ -3,33 +3,32 @@
 
 <h2>Heading level 2</h2>
 <p>
-  <?php include 'partials/text-45-75-markers.html' ?>
+    <?php include 'partials/text-45-75-markers.html' ?>
 </p>
 
 <h3>Heading level 3</h3>
-<p>This paragraph has a <a href="#">link</a> and <strong>strong</strong> and <em>emphasis</em> and <b>bold</b> and
-  <i>italics</i>.</p>
+<p><?php include 'partials/text-inline-semantics.html' ?></p>
 
 <p>
-  The paragraph is followed by this next paragraph.
-  <?php include "partials/text-lorem-ipsum.html" ?>
+    The paragraph is followed by this next paragraph.
+    <?php include "partials/text-lorem-ipsum.html" ?>
 </p>
 
 <blockquote>
-  <p>This is a blockquote. <?php include "partials/text-lorem-ipsum-short.html" ?></p>
-  <footer>Someone Famous in <cite title="Source Title">A Very Famous Book</cite></footer>
+    <p>This is a blockquote. <?php include "partials/text-lorem-ipsum-short.html" ?></p>
+    <footer>Someone Famous in <cite title="Source Title">A Very Famous Book</cite></footer>
 </blockquote>
 
 <p><cite>Someone</cite> might say <q>this is a short inline quote</q> and that would be true indeed.</p>
 
 <ul>
-  <li>An item in an unordered list</li>
-  <li>An item in an unordered list</li>
-  <li>An item in an unordered list</li>
+    <li>An item in an unordered list</li>
+    <li>An item in an unordered list</li>
+    <li>An item in an unordered list</li>
 </ul>
 
 <ol>
-  <li>An item in an ordered list</li>
-  <li>An item in an ordered list</li>
-  <li>An item in an ordered list</li>
+    <li>An item in an ordered list</li>
+    <li>An item in an ordered list</li>
+    <li>An item in an ordered list</li>
 </ol>
